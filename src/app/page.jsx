1041720edb636adc3c0c12258be2418caf619786
anis5165 <anishkumargvm02@gmail.com>
2025-07-20 14:30:06 +0530

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen bg-gradient-to-br from-slate-900 via-blue-900 to-slate-800 text-white overflow-hidden'>
+
+    </div>
+  )
+}
+
+export default Home
